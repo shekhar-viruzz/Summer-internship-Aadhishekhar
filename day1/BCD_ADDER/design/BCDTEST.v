e260693d38a9df894562912cacc5591cac68c80e
Shekhar-aadhi <aadhishekhar@gmail.com>
2026-06-09 20:47:19 +0530

    Add files via upload

---
+++ day1/BCD_ADDER/design/BCDTEST.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.06.2026 21:05:20
+// Design Name: 
+// Module Name: BCDTEST
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BCDTEST(
+
+    );
+endmodule
